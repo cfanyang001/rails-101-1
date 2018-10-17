dd84c03e847d4a0be5cb31bb12e46fcc86ffb9ae
大圣 <yyf42336@gmail.com>
2018-10-17 13:54:50 +0800

    people can't see edit button unless he is group owner

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2018_10_17_014852) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
