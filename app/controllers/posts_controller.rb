64798c7f069f260791fb7db5bc649014c80620b7
大圣 <yyf42336@gmail.com>
2018-10-17 16:22:46 +0800

    use simple_format helper to decorate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,28 +1,52 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :ipdate, :destroy]
 
-def new
-  @group = Group.find(params[:group_id])
-  @post = Post.new
-end
-
-def create
-  @group = Group.find(params[:group_id])
-  @post = Post.new(post_params)
-  @post.group = @group
-  @post.user = current_user
-
-  if @post.save
-    redirect_to group_path(@group)
-  else
-    render :new
+  def new
+    @group = Group.find(params[:group_id])
+    @post = Post.new
   end
-end
 
+  def create
+    @group = Group.find(params[:group_id])
+    @post = Post.new(post_params)
+    @post.group = @group
+    @post.user = current_user
 
-private
+    if @post.save
+      redirect_to group_path(@group)
+    else
+      render :new
+    end
+  end
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+  end
 
-def post_params
-  params.require(:post).permit(:content)
-end
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+    @post.user = current_user
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+    @post.destroy
+    redirect_to account_posts_path, alert: "Post deleted"
+  end
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
